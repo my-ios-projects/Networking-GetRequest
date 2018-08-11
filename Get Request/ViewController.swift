e7910251ce93078410e993aac7fc40c3cea96a69
TahaMagdy <tahamagdy314@gmail.com>
2018-08-12 00:47:27 +0200

    Initial Commit

---
+++ Get Request/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get Request
+//
+//  Created by Taha Magdy on 8/12/18.
+//  Copyright © 2018 Taha Magdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
