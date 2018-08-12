2067485bdb7bba8a79b81ecea03bc74c2e223f20
TahaMagdy <tahamagdy314@gmail.com>
2018-08-12 02:49:01 +0200

    some comments

--- Get Request/ViewController.swift
+++ Get Request/ViewController.swift
@@ -27,22 +27,27 @@ class ViewController: UIViewController {
         
         // send the request
         getImageFromInternet()
-
+        
     } // end sendRequestAction
     
     
     func getImageFromInternet() {
         
+        // 1* Set the URL
         let urlString = "https://www.wisdomjobs.com/tutorials/binary-search-tree-data-structure.png"
         let imageURL = URL(string: urlString)
         
         // NOTE: request is called task in iOS
+
+        // 2* Sending the request as a dataTask.
         let request_task = URLSession.shared.dataTask(with: imageURL!) {
             (receivedRowData, response, error) in
             
             print("Reqeust is DONE")
             
             if error == nil{
+                // 3* Serialize the row data that is received from the request.
+                //    Now we have the data; play with it.
                 let downloadedImage = UIImage(data: receivedRowData!)
                 
                 DispatchQueue.main.async {
